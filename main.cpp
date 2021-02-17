a4503a882ed7c6837ca17c7b18b35c0e0ece33e7
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2021-02-17 09:21:45 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <glad/glad.h>
+#define GLFW_DLL
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+using namespace glm;
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+int main()
+{
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+    }
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window;
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Tutorial 01", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    if (window == NULL)
+    {
+        fprintf(stderr, "Failed to open GLFW window");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        fprintf(stderr, "Failed to initialize OpenGL context");
+        return -1;
+    }
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+    std::cout << key << std::endl;
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
