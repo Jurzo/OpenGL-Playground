4cd4d6b00af18c6ce3c7a885db0c8e727bbbb7d6
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2021-02-19 13:51:28 +0200

    added exe to ignore

--- main.cpp
+++ main.cpp
@@ -17,8 +17,8 @@ int main()
         fprintf(stderr, "Failed to initialize GLFW\n");
     }
     glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow *window;
