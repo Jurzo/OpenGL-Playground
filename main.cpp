95f001f6409940d1595c05031d2168a210c8060d
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2021-03-09 12:39:26 +0200

    multiple cubes

--- main.cpp
+++ main.cpp
@@ -149,6 +149,20 @@ int main()
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
+    // cube model positions
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f), 
+        glm::vec3( 2.0f,  5.0f, -15.0f), 
+        glm::vec3(-1.5f, -2.2f, -2.5f),  
+        glm::vec3(-3.8f, -2.0f, -12.3f),  
+        glm::vec3( 2.4f, -0.4f, -3.5f),  
+        glm::vec3(-1.7f,  3.0f, -7.5f),  
+        glm::vec3( 1.3f, -2.0f, -2.5f),  
+        glm::vec3( 1.5f,  2.0f, -2.5f), 
+        glm::vec3( 1.5f,  0.2f, -1.5f), 
+        glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
+
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -171,7 +185,6 @@ int main()
     // 3D matrix transformations
     // model
     mat4 model = mat4(1.0f);
-    model = rotate(model, radians(-55.0f), vec3(1.0f, 1.0f, 0.0f));
     // view
     mat4 view = mat4(1.0f);
     view = translate(view, vec3(0.0f, 0.0f, -3.0f));
@@ -203,18 +216,24 @@ int main()
         ourShader.set3f("offset", offset);
         ourShader.set3f("ourColor", vec3(redValue, greenValue, blueValue)); */
 
-        // Rotate the cube over time
-        model = mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(55.0f), glm::vec3(0.0f, 0.5f, 1.0f));
-        ourShader.setmatrix4("model", model);
-
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (unsigned int i = 0; i < 10; i++) {
+            model = mat4(1.0f);
+            model = translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            if (i % 3 == 0) {
+                angle = glfwGetTime() * 20.0f;
+            }
+            model = rotate(model, radians(angle), vec3(1.0f, 0.3f, 0.5f));
+            ourShader.setmatrix4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
         
         glBindVertexArray(0);
 
